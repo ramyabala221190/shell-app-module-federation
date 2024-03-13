6b51fbe39dcbf7cdc1220756463922455a7e07d5
Ramya <ramya.bala221190@gmail.com>
2024-03-13 22:27:29 +0530

    updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { loadManifest } from '@angular-architects/module-federation';
 
-//import * as config from './assets/json/config.json'
+import config from './assets/json/config.json'
 
 // import('./bootstrap')
 // 	.catch(err => console.error(err));
 
-//console.log(config.env)
+console.log(config.env)
 
-loadManifest(`assets/manifest/mf-dev.manifest.json`,true) .catch((err) => console.error('Error loading remote entries', err))
+loadManifest(`assets/manifest/mf-${config.env}.manifest.json`,true) .catch((err) => console.error('Error loading remote entries', err))
 .then(() => import('./bootstrap')) .catch((err) => console.error(err));
 
 

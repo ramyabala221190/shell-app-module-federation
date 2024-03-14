f6fa82037a36003b91f7fbe0d3513fb01125eb37
Ramya <ramya.bala221190@gmail.com>
2024-03-14 22:50:05 +0530

    updated nginx

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cp /usr/share/nginx/html/assets/json/config-$(cat /config/env).json /usr/share/nginx/html/assets/json/config.json
-echo "Starting container. Runtime environment variables. target environment=$(cat /config/env)  ${remoteA}  ${remoteB}" 
+envsubst "${remoteA}" < /etc/nginx/conf.d/default.conf
+envsubst "${remoteB}" < /etc/nginx/conf.d/default.conf
+echo "Starting container. Runtime environment variables. target environment=$(cat /config/env)" 
 nginx -g 'daemon off;'

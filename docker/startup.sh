0a508637f95a6d0259f7d04f818e7c345756ea66
Ramya <ramya.bala221190@gmail.com>
2024-03-15 18:24:45 +0530

    updated manifest

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp /usr/share/nginx/html/assets/json/config-$(cat /config/env).json /usr/share/nginx/html/assets/json/config.json
+envsubst < /usr/share/nginx/html/assets/manifest/mf-temp.manifest.json > /usr/share/nginx/html/assets/manifest/mf.manifest.json
 envsubst < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
-#envsubst "${remoteA},${remoteB}" < /etc/nginx/conf.d/nginx.config > /etc/nginx/conf.d/default.conf
 echo "Starting container. Runtime environment variables. target environment=$(cat /config/env)" 
 nginx -g 'daemon off;'

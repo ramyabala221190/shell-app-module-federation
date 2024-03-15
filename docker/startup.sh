5633d18d1a99d75f2232dbae89fe4bbcf58d6c76
Ramya <ramya.bala221190@gmail.com>
2024-03-15 17:47:36 +0530

    updated template

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp /usr/share/nginx/html/shell/assets/json/config-$(cat /config/env).json /usr/share/nginx/html/shell/assets/json/config.json
+cp /usr/share/nginx/html/assets/json/config-$(cat /config/env).json /usr/share/nginx/html/assets/json/config.json
 #envsubst "${remoteA},${remoteB}" < /etc/nginx/conf.d/nginx.config > /etc/nginx/conf.d/default.conf
 echo "Starting container. Runtime environment variables. target environment=$(cat /config/env)" 
 nginx -g 'daemon off;'
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import {HttpClientModule} from '@angular/common/http'
-import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -17,10 +16,7 @@ import { APP_BASE_HREF } from '@angular/common';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [{
-    provide:APP_BASE_HREF,
-    useValue:'/shell/'
-  }],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

498bb7f0f244780d10f10556241d765e15acb2e8
Ramya <ramya.bala221190@gmail.com>
2024-03-15 18:38:35 +0530

    updated nginx

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,6 @@
 import { loadManifest } from '@angular-architects/module-federation';
 
-
-// import('./bootstrap')
-// 	.catch(err => console.error(err));
-//fetch('/assets/json/config.json').then(response=>response.json()).then((configuration:any)=>{
-   // console.log(configuration);
-    loadManifest(`assets/manifest/mf.manifest.json`,true)
-    .catch((err) => console.error('Error loading remote entries', err))
-    .then(() => import('./bootstrap')) .catch((err) => console.error(err));  
-// })
-// .catch(err=>{
-//     console.log(err)
-// })
-
+loadManifest(`assets/manifest/mf.manifest.json`,true)
+.catch((err) => console.error('Error loading remote entries', err))
+.then(() => import('./bootstrap')) .catch((err) => console.error(err));  
 
